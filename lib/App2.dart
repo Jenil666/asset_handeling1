064e0bb853dd6c4f18344d43bd61f64c3ce938cb
Jenil666 <jenilchandarana33@gmail.com>
2023-01-30 19:15:18 +0530

    30-1-23 7:15

--- lib/App2.dart
+++ lib/App2.dart
@@ -9,6 +9,73 @@ class Project2 extends StatefulWidget {
 }
 
 class _Project2State extends State<Project2> {
+
+  List phor =[
+    "assets/images/fashion/2.jpg",
+    "assets/images/fashion/4.jpg",
+    "assets/images/fashion/6.jpg",
+    "assets/images/fashion/8.jpg",
+    "assets/images/fashion/10.jpg",
+    "assets/images/fashion/12.jpg",
+  ];
+  List phol =[
+    "assets/images/fashion/1.png",
+    "assets/images/fashion/3.jpg",
+    "assets/images/fashion/5.jpg",
+    "assets/images/fashion/7.jpg",
+    "assets/images/fashion/9.jpg",
+    "assets/images/fashion/11.jpg",
+  ];
+  List ratingl = [
+    "5.0",
+    "4.9",
+    "4.7",
+    "4.8",
+    "4.2",
+    "4.3",
+  ];
+
+  List ratingr = [
+    "4.1",
+    "4.2",
+    "4.5",
+    "4.8",
+    "4.1",
+    "4.0",
+  ];
+
+  List pricel = [
+    "\$30.33",
+    "\$4.00",
+    "\$70.00",
+    "\$56.27",
+    "\$90.99",
+    "\$99.99",
+  ];
+  List pricer = [
+    "\$52.00",
+    "\$99.99",
+    "\$72.30",
+    "\$60.90",
+    "\$45.90",
+    "\$25.33",
+  ];
+  List namel = [
+    "SHOES",
+    "TOP",
+    "HODIE",
+    "COMBO",
+    "SHRUG",
+    "WATCH",
+  ];
+  List namer = [
+    "TSHIRT",
+    "BLAZER",
+    "JEANS",
+    "JAKET",
+    "HOT WEAR",
+    "SHIRT",
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,21 +98,21 @@ class _Project2State extends State<Project2> {
                 ]
               )
             ),
-            child: Column(
-              children: [
-                Cont(),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: phol.asMap().entries.map((e) => Cont(phol[e.key], phor[e.key],ratingl[e.key],ratingr[e.key],pricel[e.key],pricer[e.key],namel[e.key],namer[e.key])).toList(),
+              ),
             ),
           ),
      ),
     );
   }
 
-  Widget Cont()
+  Widget Cont(String phl,String phr,String ratl,String ratr,String pril,String prir,String naml,String namr)
   {
     return Column(
       children: [
-        SizedBox(height: 10,),
+        SizedBox(height: 30,),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -57,7 +124,7 @@ class _Project2State extends State<Project2> {
                   width: 150,
                   child: ClipRRect(
                       borderRadius: BorderRadius.circular(10),
-                      child: Image.asset("assets/images/fashion/1.png",fit: BoxFit.fill,)),
+                      child: Image.asset("$phl",fit: BoxFit.fill,)),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10)
                   ),
@@ -70,9 +137,9 @@ class _Project2State extends State<Project2> {
                     child: Row(
                       children: [
                         SizedBox(width: 5,),
-                        Text("SHOES",style: TextStyle(color: Colors.white,fontSize: 13),),
+                        Text("$naml",style: TextStyle(color: Colors.white,fontSize: 13),),
                         Expanded(child: SizedBox(height: 3,)),
-                        Text("\$30.33",style: TextStyle(color: Colors.white,fontSize: 13),),
+                        Text("$pril",style: TextStyle(color: Colors.white,fontSize: 13),),
                         SizedBox(width: 5,),
                       ],
                     ),
@@ -93,7 +160,7 @@ class _Project2State extends State<Project2> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("5.0",style: TextStyle(color: Colors.white,fontSize: 12),),
+                        Text("$ratl",style: TextStyle(color: Colors.white,fontSize: 12),),
                         Icon(Icons.star,color: Colors.white,size: 15,)
                       ],
                     ),
@@ -108,13 +175,64 @@ class _Project2State extends State<Project2> {
                 ),
               ],
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 170,
-              width: 150,
-              color: Colors.red,
-              child: Image.asset("assets/images/fashion/2.jpg",fit: BoxFit.fill,),
-
+            Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 10,right: 5,),
+                  height: 170,
+                  width: 150,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset("$phr",fit: BoxFit.fill,)),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10,top: 140),
+                  child: Container(
+                    height: 30,
+                    width: 150,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 5,),
+                        Text("$namr",style: TextStyle(color: Colors.white,fontSize: 13),),
+                        Expanded(child: SizedBox(height: 3,)),
+                        Text("$prir",style: TextStyle(color: Colors.white,fontSize: 13),),
+                        SizedBox(width: 5,),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        )
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.2,top: 0.1),
+                  child: Container(
+                    height: 20,
+                    width: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("$ratr",style: TextStyle(color: Colors.white,fontSize: 12),),
+                        Icon(Icons.star,color: Colors.white,size: 15,)
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: CupertinoColors.systemGreen,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        )
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-
 import 'package:asset_handeling/App1.dart';
 import 'package:asset_handeling/App2.dart';
+import 'package:asset_handeling/phote1.dart';
+import 'package:asset_handeling/photo2.dart';
 import 'package:flutter/material.dart';
 
 void main()
@@ -8,10 +9,12 @@ void main()
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'aps1',
+      initialRoute: 'ph2',
       routes: {
         '/':(context)=>Ceolist(),
         'aps1':(context)=>Project2(),
+        'ph1':(context)=>Photo1(),
+        'ph2':(context)=>Photo2(),
       },
     ),
   );
