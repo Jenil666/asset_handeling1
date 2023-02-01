66d0a2a6de41519448b4d07be528e774709e987f
arpitaflutter <arpitamangukiya2000@gmail.com>
2023-02-01 12:24:47 +0530

    31-1-23 12:30

---
+++ lib/Pgotos/project7.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Project7 extends StatefulWidget {
+  const Project7({Key? key}) : super(key: key);
+
+  @override
+  State<Project7> createState() => _Project7State();
+}
+
+class _Project7State extends State<Project7> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      appBar: AppBar(),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+
 import 'package:asset_handeling/App/App1.dart';
 import 'package:asset_handeling/Pgotos/photo1.dart';
 import 'package:asset_handeling/Pgotos/photo3.dart';
 import 'package:asset_handeling/Pgotos/photo4.dart';
 import 'package:asset_handeling/Pgotos/photo5A.dart';
+import 'package:asset_handeling/Pgotos/photo6.dart';
 import 'package:asset_handeling/Pgotos/photp5B.dart';
+import 'package:asset_handeling/Pgotos/project7.dart';
 import 'package:flutter/material.dart';
 
 import 'App/App2.dart';
@@ -14,7 +17,7 @@ void main()
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'ph5b',
+      initialRoute: 'ph6=7',
       routes: {
         '/':(context)=>Ceolist(),
         'aps1':(context)=>Project2(),
@@ -24,6 +27,8 @@ void main()
         'ph4':(context)=>Photo4(),
         'ph5a':(context)=>Project5A(),
         'ph5b':(context)=>Photo5B(),
+        'ph6':(context)=>Photo6(),
+        'ph7':(context)=>Project7(),
       },
     ),
   );
